83bad78989449653e9c71c41e2fc93ff103f7b06
Kyle Herman <162386221+TheHermanator77@users.noreply.github.com>
2025-12-15 22:36:35 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
+// db.js
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "student-databases.cvode4s4cwrc.us-west-2.rds.amazonaws.com",
-  user: "kyleherman",
-  password: 'CQnB2CQREOWABG8rLO5bJYt8DvgPQgrILwZ',
-  database: "kyleherman",
-  port: 3306,
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT || 3306),
   charset: "utf8mb4",
-
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 module.exports = pool;
